c3f5afc01bc2983742ea98dc3d0ff08ec34bc1e9
GabinLeveugle <Gabin.Leveugle@etu.univ-savoie.fr>
2023-01-04 08:40:50 +0100

    scene continuation

--- SAE_DEV/SAE_DEV/Game1.cs
+++ SAE_DEV/SAE_DEV/Game1.cs
@@ -8,6 +8,7 @@ using MonoGame.Extended.Content;
 using MonoGame.Extended.Serialization;
 using MonoGame.Extended.Sprites;
 using MonoGame.Extended.Screens;
+using MonoGame.Extended.Screens.Transitions;
 
 namespace SAE_DEV
 {
@@ -18,22 +19,11 @@ namespace SAE_DEV
         Acceuil _acceuil;
         Jeu _jeu;
         Fin _fin;
-        private GraphicsDeviceManager _graphics;
-        private SpriteBatch _spriteBatch;
-        private TiledMap _tiledMap;
-        private TiledMapRenderer _tiledMapRenderer;
-        private Vector2 _positionPerso;
-        private AnimatedSprite _perso;
-        private KeyboardState _keyboardState;
-        private int _sensPerso;
-        private int _vitessePerso;
-        public const int TAILLE_FENETRE = 640;
+        
 
         public Game1()
         {
-            _graphics = new GraphicsDeviceManager(this);
-            Content.RootDirectory = "Content";
-            IsMouseVisible = true;
+            
             _screenManager = new ScreenManager();
             Components.Add(_screenManager);
         }
@@ -41,10 +31,7 @@ namespace SAE_DEV
         protected override void Initialize()
         {
             GraphicsDevice.BlendState = BlendState.AlphaBlend;
-            _positionPerso = new Vector2(20, 340);
-            _graphics.PreferredBackBufferHeight = TAILLE_FENETRE;
-            _graphics.ApplyChanges();
-            _vitessePerso = 600;
+            
             base.Initialize();
         }
 
@@ -53,11 +40,7 @@ namespace SAE_DEV
             _acceuil = new Acceuil(this); // en leur donnant une référence au Game
             _jeu = new Jeu(this);
             _fin = new Fin(this);
-            _spriteBatch = new SpriteBatch(GraphicsDevice);
-            _tiledMap = Content.Load<TiledMap>("mapGenerale");
-            _tiledMapRenderer = new TiledMapRenderer(GraphicsDevice, _tiledMap);
-            SpriteSheet spriteSheet = Content.Load<SpriteSheet>("persoAnimation.sf", new JsonContentLoader());
-            _perso = new AnimatedSprite(spriteSheet);
+            
 
 
             // TODO: use this.Content to load your game content here
@@ -65,47 +48,24 @@ namespace SAE_DEV
 
         protected override void Update(GameTime gameTime)
         {
-            /*KeyboardState keyboardState = Keyboard.GetState();
+            KeyboardState keyboardState = Keyboard.GetState();
             if (keyboardState.IsKeyDown(Keys.Left))
             {
-                _screenManager.LoadScreen(_myScreen1, new FadeTransition(GraphicsDevice,
+                _screenManager.LoadScreen(_acceuil, new FadeTransition(GraphicsDevice,
                 Color.Black));
+            }
+            else if (keyboardState.IsKeyDown(Keys.Down))
+            {
+                _screenManager.LoadGame(_jeu, new FadeTransition(GraphicsDevice,Color.Black));
             }
             else if (keyboardState.IsKeyDown(Keys.Right))
             {
-                _screenManager.LoadScreen(_myScreen2, new FadeTransition(GraphicsDevice,
+                _screenManager.LoadScreen(_fin, new FadeTransition(GraphicsDevice,
                 Color.Black));
-            }*/
-            float deltaSeconds = (float)gameTime.ElapsedGameTime.TotalSeconds;
-            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
-                Exit();
+            }
+            
 
-            // TODO: Add your update logic here
-            _tiledMapRenderer.Update(gameTime);
-            _perso.Play("idle"); // une des animations définies dans « persoAnimation.sf »
-            _perso.Update(deltaSeconds); // time écoulé 
-            _keyboardState = Keyboard.GetState();
-            if (_keyboardState.IsKeyDown(Keys.Right) && !(_keyboardState.IsKeyDown(Keys.Left)))
-            {
-                _sensPerso = 1;
-                _positionPerso.X += _sensPerso * _vitessePerso * deltaSeconds;
-            }
-            // si fleche gauche
-            else if (_keyboardState.IsKeyDown(Keys.Left) && !(_keyboardState.IsKeyDown(Keys.Right)))
-            {
-                _sensPerso = 1;
-                _positionPerso.X -= _sensPerso * _vitessePerso * deltaSeconds;
-            }
-            if (_keyboardState.IsKeyDown(Keys.Down) && !(_keyboardState.IsKeyDown(Keys.Up)))
-            {
-                _sensPerso = 1;
-                _positionPerso.Y += _sensPerso * _vitessePerso * deltaSeconds;
-            }
-            else if (_keyboardState.IsKeyDown(Keys.Up) && !(_keyboardState.IsKeyDown(Keys.Down)))
-            {
-                _sensPerso = 1;
-                _positionPerso.Y -= _sensPerso * _vitessePerso * deltaSeconds;
-            }
+           
             base.Update(gameTime);
         }
     
@@ -114,11 +74,8 @@ namespace SAE_DEV
         {
             GraphicsDevice.Clear(Color.CornflowerBlue);
 
-            // TODO: Add your drawing code here
-            _tiledMapRenderer.Draw();
-            _spriteBatch.Begin();
-            _spriteBatch.Draw(_perso, _positionPerso);
-            _spriteBatch.End();
+            
+            
             base.Draw(gameTime);
         }
     }
